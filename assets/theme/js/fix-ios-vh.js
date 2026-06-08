ebdf182956548f3a51c8699b9d12eb18ddce7150
Kieran Nolan <kdnolan@gmail.com>
2026-06-08 11:46:08 +1000

    new site

--- assets/theme/js/fix-ios-vh.js
+++
@@ -1,9 +0,0 @@
-function setVh() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-setVh();
-
-window.addEventListener('resize', setVh);
-window.addEventListener('orientationchange', setVh);
